6b250624fe5fceb1ed78af78f6e3f4a0c8ebec74
Tomaz Toplak <tomaz@cvar.si>
2013-08-18 18:12:50 +0200

    Showing longer data in popups.
    All the fields that contain long data are now truncated and the whole content is shown on click.
    Added jQuery and modalPopLite.

---
+++ Web/Models/StringHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GitDeployHub.Web
+{
+    public static class StringExt
+    {
+        public static string Truncate(this string value, int maxLength)
+        {
+            return value.Length <= maxLength ? value : value.Substring(0, maxLength);
+        }
+    }
+}
